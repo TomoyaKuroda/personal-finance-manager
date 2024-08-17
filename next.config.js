baf459c690c6533cebc04904267b412bce170404
Tomoya Kuroda <tomoya@impactnorth.com>
2024-08-17 14:33:41 -0400

    remove now.json

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 module.exports = {
-  env: {
-    // MONGODB_URI: process.env.MONGODB_URI,
-    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
-    DB_NAME: process.env.DB_NAME,
-    // WEB_URI: process.env.WEB_URI,
-    // SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
-    // SENDGRID_TEMPLATEID_EMAILVERIFY: process.env.SENDGRID_TEMPLATEID_EMAILVERIFY,
-    // EMAIL_FROM: process.env.EMAIL_FROM,
-  },
+  // env: {
+  //   // MONGODB_URI: process.env.MONGODB_URI,
+  //   CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+  //   DB_NAME: process.env.DB_NAME,
+  //   // WEB_URI: process.env.WEB_URI,
+  //   // SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  //   // SENDGRID_TEMPLATEID_EMAILVERIFY: process.env.SENDGRID_TEMPLATEID_EMAILVERIFY,
+  //   // EMAIL_FROM: process.env.EMAIL_FROM,
+  // },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
